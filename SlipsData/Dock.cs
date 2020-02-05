1ce20819b040f37e6e27508df8385d18cda79108
Elias Nahas <elias.nahas@gmail.com>
2020-02-05 13:51:16 -0700

    DB Objects and Classes
    Created connection to Marina
    Created classes Slip and Dock
    Created db objects for Slip and Dock

---
+++ SlipsData/Dock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SlipsData
+{
+    public class Dock
+    {
+        public Dock() { }
+
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public bool WaterService { get; set; }
+
+        public bool ElectricalService { get; set; }
+    }
+}
